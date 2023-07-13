567de632ddf074fb586cac2fc01c68b8704a5c4d
jantgg <smosh360@hotmail.com>
2023-07-13 20:07:22 +0000

    hola

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -55,6 +55,7 @@ export const Home = () => {
     backgroundImage: `url(${numerogris})`,
     backgroundSize: "cover",
     backgroundPosition: "center center",
+    zIndex: "-2",
   };
 
   return (
@@ -94,25 +95,49 @@ export const Home = () => {
       <div
         id="start"
         ref={startRef}
-        className="d-flex col-12 flex-column section-1"
+        className="d-flex col-12 flex-column section-1 margin-top-section1"
       >
-        <div className="col-12 d-flex flex-column" style={stylesbackground1}>
-          <div className="col-8 mx-auto d-flex">
-            <div className="col-4">
+        <div className="col-12 d-flex flex-column">
+          <div id="fancy-borders" className="col-8 mx-auto d-flex p-4">
+            <div className="col-5 d-flex flex-column">
               {" "}
-              <img src={cruzado} style={{ width: "100%", height: "100%" }} />
-            </div>
-            <div className="col-8 d-flex flex-cloumn">
-              <h1 className="text-black">¿Quienes somos?</h1>
-              <h2 className="text-black">
-                ... No somos el tipico taller oficial
-              </h2>
-              <p className="text-black">
+              <img src={cruzado} style={{ width: "100%", height: "" }} />
+              <div className="col-12 text-black border-red-r pb-5 mt-5">
                 {" "}
-                En llave 13 contamos con más de 20 años de experiencia en el
-                sector de la mecánica
+                <p className="comment col-10 ms-auto pe-4 pt-3 border-red-r-2">
+                  "Nuestra promesa es brindarte la tranquilidad de confiar en un
+                  taller profesional y comprometido que cuidará de tu vehículo
+                  con la seriedad y la implicación que merece." <br></br>{" "}
+                  <strong>-- Moi Benitez</strong>
+                </p>
+                <p className="comment col-10 ms-auto pe-4 pt-3"></p>
+              </div>
+            </div>
+            <div className="col-7 d-flex flex-column ps-5">
+              <h1 className="col-12 who-h1 pt-5">¿Quiénes somos?</h1>
+              <h2 className="col-12 who-h2 pt-3">
+                ... No somos el típico taller oficial
+              </h2>
+              <p className="col-12 who-p pe-5 pt-5">
+                {" "}
+                En{" "}
+                <span className="text-my-red ">
+                  <strong>llave 13</strong>
+                </span>
+                , somos un equipo de profesionales apasionados por la mecánica y
+                comprometidos en brindarte el mejor servicio para tu vehículo.
+                Nos destacamos por nuestra profesionalidad y seriedad, rompiendo
+                con los estereotipos de los talleres convencionales. Nuestra
+                vocación por ayudarte nos impulsa a ofrecerte soluciones
+                personalizadas y cercanas a tus necesidades. Nos implicamos
+                totalmente en cada proyecto, dedicando la atención y el cuidado
+                que tu vehículo merece.<br></br>{" "}
               </p>
-              <p className="text-black"></p>
+              <p className="col-12 who-p pe-5 pb-5">
+                Tu tranquilidad es nuestra prioridad, y nos esforzamos en
+                ofrecerte resultados de calidad respaldados por años de
+                experiencia y herramientas de calidad.
+              </p>
             </div>
           </div>
         </div>
